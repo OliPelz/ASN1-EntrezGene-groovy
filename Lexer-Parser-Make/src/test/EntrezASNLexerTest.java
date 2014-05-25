87d6ece43f113371403e0437bba798f19453d931
olip <olip@olips-MacBook.local>
2014-05-25 09:40:23 +0200

    finished structuring project for makefile and started writing test

---
+++ Lexer-Parser-Make/src/test/EntrezASNLexerTest.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import junit.framework.TestCase;
+import de.oliverpelz.*;
+
+/**
+ *
+ * this is the main test class for the parser lexer combination for ASN Entrez 
+ */
+public class EntrezASNLexerTest extends TestCase {
+    
+    @Test
+    public void testConstructing() {
+       fail("cannot construct the Entrez ASN parser and lexer objects");
+    }
+}
