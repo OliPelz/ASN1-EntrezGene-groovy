cd3547467410b7070cbee9daa56304b6b33be485
Oliver Pelz <o.pelz@dkfz.de>
2014-05-26 09:07:54 +0200

    test case for the Parser is now working - finally (had to mess around javac -sourcepath etc) introduced junit4 new testrunner called : org.junit.runner.JUnitCore

---
+++ Lexer-Parser-Make/src/test/EntrezASNParserTest.java
@@ -0,0 +1,18 @@
+import de.oliverpelz.*;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * this is the main test class for the parser lexer combination for ASN Entrez 
+ */
+public class EntrezASNParserTest {
+    
+    @Test
+    public void testConstructing() {
+       fail("cannot construct the Entrez ASN parser and lexer objects");
+    }
+}
